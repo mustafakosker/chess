4c08dbff075bbbc50b45c3ea6f5f6a8b471290da
kosker <mustafakosker@gmail.com>
2016-07-02 17:27:19 +0300

    CellStatus and Position models added
    - CellStatus holds the status of each cell
    - Position holds x and y position of piece

---
+++ src/main/java/com/trycatch/chess/constants/CellStatus.java
@@ -0,0 +1,22 @@
+package com.trycatch.chess.constants;
+
+/**
+ * Indicates the status of an each cell in the board.
+ */
+public final class CellStatus {
+
+    /** EMPTY means the cell is empty and any kind of piece
+     *  can be placed in it. */
+    public static final int EMPTY = 0;
+
+    /**
+     * OCCUPIED means the cell is threatened by another piece
+     * and nothing can be placed in it.
+     */
+    public static final int OCCUPIED = 1;
+
+    /**
+     * FILLED means there is a piece in the cell.
+     */
+    public static final int FILLED = 2;
+}
