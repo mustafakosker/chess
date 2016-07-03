1339dbd6a068cc79e9ef033f96c6c4d817dd663b
kosker <mustafakosker@gmail.com>
2016-07-03 03:51:30 +0300

    getNextAvailablePosition method
    - method to calculate the closest empty cell
    to given position added along with the tests.

--- src/main/java/com/trycatch/chess/model/Board.java
+++ src/main/java/com/trycatch/chess/model/Board.java
@@ -34,7 +34,7 @@ public class Board {
         final int x = position.getX();
         final int y = position.getY();
 
-        if (x >= width || y >= height || x < 0 || y < 0 ||
+        if (!isPositionValid(x, y) ||
                 boardData[x][y] != EMPTY) {
             return false;
         } else {
@@ -50,5 +50,30 @@ public class Board {
      */
     public void setCellStatus(Position position, int cellStatus) {
         boardData[position.getX()][position.getY()] = cellStatus;
+    }
+
+    /**
+     * Calculates and returns the closest empty cell on the board.
+     *
+     * @param position to start from
+     * @return the empty position closest to given position.
+     */
+    public Position getNextAvailablePosition(Position position) {
+        final int x = position.getX();
+        final int y = position.getY();
+
+        if (!isPositionValid(x, y)) {
+            return null;
+        } else if (boardData[x][y] == EMPTY) {
+            return position;
+        } else if (x < width - 1) {
+            return getNextAvailablePosition(new Position(x + 1, y));
+        } else {
+            return getNextAvailablePosition(new Position(0, y + 1));
+        }
+    }
+
+    private boolean isPositionValid(int x, int y) {
+        return (x < width) && (y < height) && (x >= 0) && (y >= 0);
     }
 }
--- src/test/java/com/trycatch/chess/model/BoardTest.java
+++ src/test/java/com/trycatch/chess/model/BoardTest.java
@@ -3,8 +3,7 @@ package com.trycatch.chess.model;
 import org.junit.Test;
 
 import static com.trycatch.chess.constants.CellStatus.OCCUPIED;
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.*;
 
 /**
  * Created by kosker on 03/07/16.
@@ -33,5 +32,52 @@ public class BoardTest {
         final Board board = new Board(3, 3);
 
         assertTrue("EMPTY cell should be available", board.isCellAvailable(position));
+    }
+
+    @Test
+    public void getNextAvailablePositionTestNegativePosition() throws Exception {
+        final Position position = new Position(-1, -1);
+        final Board board = new Board(2, 2);
+
+        assertNull("Next available position to invalid position should be null",
+                board.getNextAvailablePosition(position));
+    }
+
+    @Test
+    public void getNextAvailablePositionTestEdgePosition() throws Exception {
+        final Position position = new Position(3, 3);
+        final Board board = new Board(3, 3);
+
+        assertNull("Next available position to out of board position should be null",
+                board.getNextAvailablePosition(position));
+    }
+
+    @Test
+    public void getNextAvailablePositionTestEmptyBoard() throws Exception {
+        final Position position = new Position(0, 0);
+        final Board board = new Board(2, 2);
+
+        assertEquals("Next available position to an empty position should be itself", position,
+                board.getNextAvailablePosition(position));
+    }
+
+    @Test
+    public void getNextAvailablePositionTestOccupiedCell() throws Exception {
+        final Position position = new Position(1, 2);
+        final Board board = new Board(3, 3);
+        board.setCellStatus(position, OCCUPIED);
+
+        assertEquals("Next available position to occupied cell should be adjecent cell", new Position(2, 2),
+                board.getNextAvailablePosition(position));
+    }
+
+    @Test
+    public void getNextAvailablePositionTestOccupiedCellHorizontalLastCell() throws Exception {
+        final Position position = new Position(2, 1);
+        final Board board = new Board(3, 3);
+        board.setCellStatus(position, OCCUPIED);
+
+        assertEquals("Next available position to occupied cell in the last horizontal cell should be the first cell in" +
+                "the next row", new Position(0, 2), board.getNextAvailablePosition(position));
     }
 }
